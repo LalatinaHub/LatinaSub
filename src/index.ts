3460382113b1c4ea6f4f048510b0ed2304c492b0
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-01-10 07:43:30 +0700

    Set max result

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ class Main {
   private configUrls: string[] = [];
   private connectCount: number = 0;
   private maxConcurrentTest: number = 50;
-  private maxResult: number = 50;
+  private maxResult: number = 500;
   private numberOfTest: number = 2;
   private fishermanObjects: FishermanType[] = [];
 
